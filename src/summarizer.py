4e446350eb641bd90e714c7c3d738272bd9a17bd
Sultan Mahmood Riaz <sultanmr@hotmail.com>
2025-08-20 18:04:14 +0200

    update

--- src/summarizer.py
+++ src/summarizer.py
@@ -2,3 +2,15 @@ import os
 import openai
 import semantic_kernel as sk
 from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
+
+api_key, _ = sk.openai_settings_from_dot_env()
+kernel = sk.Kernel()
+kernel.add_text_completion_service(
+    "dv",
+    OpenAIChatCompletion("gpt-4", api_key=api_key)
+)
+prompt = kernel.create_semantic_function(
+"""
+print hello world
+""")
+print (prompt())
