c24f937af6bebe22a797faa1d17508cccf4d0244
Sultan Mahmood Riaz <sultanmr@hotmail.com>
2025-08-22 06:17:23 +0200

    update

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
 from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from dotenv import load_dotenv
+load_dotenv()
+loader = WebBaseLoader("https://en.wikipedia.org/wiki/Tea")
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0, separator="\n" )
+texts = text_splitter.split_documents(documents)
+embeddings = OpenAIEmbeddings()
+docsearch = FAISS.from_documents(texts, embeddings)
+qa = RetrievalQA.from_chain_type(
+    llm=ChatOpenAI(model_name="gpt-3.5-turbo"),
+    chain_type="stuff",
+    retriever=docsearch.as_retriever())
+
+print (qa.run("What is tea?"))
